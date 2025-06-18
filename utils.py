f8edac89db91d1c06aaf2022ce6db8169ae3a145
AbdulHamid31 <abdhamid3105@students.amikom.ac.id>
2025-06-18 10:43:47 +0800

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,21 +1,18 @@
-
 import face_recognition
 import numpy as np
 import json
 
 def get_embedding(image):
-    rgb_img = image[:, :, ::-1]
+    rgb_img = image[:, :, ::-1]  # BGR to RGB
     encodings = face_recognition.face_encodings(rgb_img)
     return encodings[0] if encodings else None
 
-def cosine_similarity(vec1, vec2):
-    vec1 = np.array(vec1)
-    vec2 = np.array(vec2)
-    return np.dot(vec1, vec2) / (np.linalg.norm(vec1) * np.linalg.norm(vec2))
-
 def load_knowledge_base(path="knowledge_base.json"):
-    with open(path, "r") as f:
-        return json.load(f)
+    try:
+        with open(path, "r") as f:
+            return json.load(f)
+    except FileNotFoundError:
+        return []
 
 def save_knowledge_base(kb, path="knowledge_base.json"):
     with open(path, "w") as f:
